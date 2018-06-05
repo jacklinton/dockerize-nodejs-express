0e108526532aa8545d49eef72dba2ef49303cf0f
Jack Linton <jacklinton@mac.com>
2018-06-05 15:39:03 -0400

    Typo.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express;
+var express = require('express');
+var app = express();
 
 app.get('/', function(req, res) {
     res.json({ 'msg': 'Hello World!'})
 });
 
-const port = process.env.port || 3000;
+var port = process.env.port || 3000;
 app.listen(port, function () {
     console.log('Server started...')
 });
